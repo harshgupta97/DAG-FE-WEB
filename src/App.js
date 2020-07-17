e34d5c33447227df0316322cfacd6d7e698a91ac
vibhor <vibhor@emproto.com>
2020-07-17 18:11:54 +0530

    Feat:Added Bootstrap and Material UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container-fluid height-100">
+        <div className="row height-70">
+          <div className="col-12 m-0">
+            <h2>Juntrax DAG Graph</h2>
+          </div>
+        </div>
+      </div>
   );
 }
 
